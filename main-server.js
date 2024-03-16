8e489dca5756ebd1dc552965e574826a0d36a93f
Si3ver <izhouwl@163.com>
2024-03-17 02:10:11 +0800

    feat: nginx & mkcert

--- main-server.js
+++ main-server.js
@@ -7,7 +7,6 @@ import cookieParser from "cookie-parser";
 const app = express();
 const { port, host } = config;
 
-// 打印请求日志
 app.use(morgan("dev"));
 
 // cookie 中间件
@@ -16,31 +15,29 @@ app.use(cookieParser());
 app.use(express.static(path.join("public", "main")));
 
 app.post("/microapps", function (req, res) {
-
+    
+  // 再次刷新页面时观察 Cookie 的携带情况
   console.log("main cookies: ", req.cookies);
 
   // 设置一个响应的 Cookie 数据
   res.cookie("main-app", true);
 
-  // 这里可以是管理后台新增菜单后存储到数据库的数据
-  // 从而可以通过管理后台动态配置微应用的菜单
   res.json([
     {
       name: "micro1",
       id: "micro1",
-      // 自定义元素名称
       customElement: "micro-app-1",
-      // 这里暂时以一个入口文件为示例
-      script: `http://${host}:${port.micro}/micro1.js`,
-      style: `http://${host}:${port.micro}/micro1.css`,
+      // 更改微应用资源的加载地址，使用 Nginx 的代理地址
+      script: `https://${host}:3001/micro1.js`,
+      style: `https://${host}:3001/micro1.css`,
       prefetch: true,
     },
     {
       name: "micro2",
       id: "micro2",
       customElement: "micro-app-2",
-      script: `http://${host}:${port.micro}/micro2.js`,
-      style: `http://${host}:${port.micro}/micro2.css`,
+      script: `https://${host}:3001/micro2.js`,
+      style: `https://${host}:3001/micro2.css`,
       prefetch: true,
     },
   ]);
--- micro-server.js
+++ micro-server.js
@@ -11,13 +11,13 @@ app.use(morgan("dev"));
 // cookie 中间件
 app.use(cookieParser());
 
-// 设置支持跨域请求头
 app.use((req, res, next) => {
   // 跨域请求中涉及到 Cookie 信息传递，值不能为 *，必须是具体的地址信息
-  res.header("Access-Control-Allow-Origin", `http://ziyi.com:${port.main}`);
+  // 跨域白名单配置为主应用的 Nginx 代理地址
+  res.header("Access-Control-Allow-Origin", `https://ziyi.com:4001`);
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Allow", "GET, POST, OPTIONS");
-  // 允许跨域请求时携带 Cookie
+  // 允许客户端发送跨域请求时携带 Cookie
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
@@ -30,10 +30,15 @@ app.use(
 );
 
 app.post("/cors", function (req, res) {
-  console.log("micro cookies: ", req.cookies);
+    
+  // 用于观察二次请求时的 Cookie 的携带情况
+  console.log('micro cookies: ', req.cookies);
 
+  // 增加 SameSite 和 Secure 属性，从而使浏览器支持 iframe 子应用的跨站携带 Cookie
+  // 注意 Secure 需要 HTTPS 协议的支持
+  const cookieOptions = { sameSite: "none", secure: true };
   // 设置一个响应的 Cookie 数据
-  res.cookie("micro-app", true);
+  res.cookie("micro-app", true, cookieOptions);
   res.json({
     hello: "true",
   });
